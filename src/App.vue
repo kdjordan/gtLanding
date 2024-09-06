ddbced4545e237766a4775b19e77c86858410f94
kdjordan <k.dean.jordan@gmail.com>
2024-09-05 17:28:31 -0700

    fixing full width

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app" class="tracking-wide">
+	<div id="app" class="tracking-wide w-full">
 		<NavBar />
 
 		<HeroSection />
@@ -15,13 +15,16 @@
 <script setup>
 	import NavBar from './components/NavBar.vue';
 	import HeroSection from './components/HeroSection.vue';
-	import StatsSection from './components/StatsSection.vue';
 	import FeaturesSection from './components/FeaturesSection.vue';
-	import PricingSection from './components/PricingSection.vue';
 	import TestimonialsSection from './components/TestimonialsSection.vue';
 	import FooterSection from './components/FooterSection.vue';
 </script>
 
 <style>
+	#app {
+		width: 100%;
+		overflow-x: hidden;
+	}
+
 	/* Remove any default styles here */
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,16 @@ export default {
         display: ['Montserrat', 'sans-serif'],
       },
       // ... existing backgroundImage config
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '100%',
+          md: '100%',
+          lg: '1024px',
+          xl: '1280px',
+        },
+      },
     },
   },
   plugins: [
